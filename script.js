3c0f4b848f259bff1235f5c60c5516952cec0202
elena_e <elenaka123@gmail.com>
2024-03-22 13:56:03 -0500

    Inital commit

---
+++ script.js
@@ -0,0 +1,19 @@
+var weekday = new Date().toLocaleString("en-US", { weekday: "long" });
+
+// Your code here
+// console.log(weekday);
+
+if (weekday === "Monday") {
+    console.log("Let's get this week started!");
+} else if (weekday === "Monday") {
+    console.log("Worky work!.");
+} else if (weekday === "Monday") {
+    console.log("Middle of the week!");
+} else if (weekday === "Monday") {
+    console.log("Almost Friday!");
+} else if (weekday === "Monday") {
+    console.log("Weekend's almost here!");
+} else {console.log(
+    "WEEKEND, YAYYY!!"
+);
+}
